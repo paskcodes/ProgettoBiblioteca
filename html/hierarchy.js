85baa99be11af0f96a5efdea1ba3145aeb4a870b
Antonio <antoniofrancogg@gmail.com>
2025-12-10 10:58:59 +0100

    Add files via upload

---
+++ html/hierarchy.js
@@ -0,0 +1,52 @@
+var hierarchy =
+[
+    [ "Application", null, [
+      [ "appbibliotecauniversitaria.BiblioManager", "classappbibliotecauniversitaria_1_1_biblio_manager.html", null ]
+    ] ],
+    [ "com.libraryproject.AppTest", "classcom_1_1libraryproject_1_1_app_test.html", null ],
+    [ "appbibliotecauniversitaria.Archivio", "classappbibliotecauniversitaria_1_1_archivio.html", null ],
+    [ "Comparable", null, [
+      [ "gestionelibro.Libro", "classgestionelibro_1_1_libro.html", null ],
+      [ "gestioneprestito.Prestito", "classgestioneprestito_1_1_prestito.html", null ],
+      [ "gestioneutente.Utente", "classgestioneutente_1_1_utente.html", null ]
+    ] ],
+    [ "Comparator", null, [
+      [ "gestionelibro.ComparatoreTitoloLibro", "classgestionelibro_1_1_comparatore_titolo_libro.html", null ],
+      [ "gestioneutente.ComparatoreCognomeNomeUtente", "classgestioneutente_1_1_comparatore_cognome_nome_utente.html", null ]
+    ] ],
+    [ "Exception", null, [
+      [ "gestionelibro.eccezioni.LibroInvalidoException", "classgestionelibro_1_1eccezioni_1_1_libro_invalido_exception.html", [
+        [ "gestionelibro.eccezioni.LibroDataPubblicazioneException", "classgestionelibro_1_1eccezioni_1_1_libro_data_pubblicazione_exception.html", null ],
+        [ "gestionelibro.eccezioni.LibroDuplicatoException", "classgestionelibro_1_1eccezioni_1_1_libro_duplicato_exception.html", null ],
+        [ "gestionelibro.eccezioni.LibroNumeroCopieException", "classgestionelibro_1_1eccezioni_1_1_libro_numero_copie_exception.html", null ]
+      ] ],
+      [ "gestionelibro.eccezioni.LibroPrestitoAttivoException", "classgestionelibro_1_1eccezioni_1_1_libro_prestito_attivo_exception.html", null ],
+      [ "gestioneprestito.eccezioni.PrestitoInvalidoException", "classgestioneprestito_1_1eccezioni_1_1_prestito_invalido_exception.html", [
+        [ "gestioneprestito.eccezioni.PrestitoCopieEsauriteException", "classgestioneprestito_1_1eccezioni_1_1_prestito_copie_esaurite_exception.html", null ],
+        [ "gestioneprestito.eccezioni.PrestitoDataScadenzaException", "classgestioneprestito_1_1eccezioni_1_1_prestito_data_scadenza_exception.html", null ],
+        [ "gestioneprestito.eccezioni.PrestitoDuplicatoException", "classgestioneprestito_1_1eccezioni_1_1_prestito_duplicato_exception.html", null ],
+        [ "gestioneprestito.eccezioni.PrestitoLimitePrestitiRaggiuntoException", "classgestioneprestito_1_1eccezioni_1_1_prestito_limite_prestiti_raggiunto_exception.html", null ]
+      ] ],
+      [ "gestioneutente.eccezioni.UtenteInvalidoException", "classgestioneutente_1_1eccezioni_1_1_utente_invalido_exception.html", [
+        [ "gestioneutente.eccezioni.UtenteDuplicatoException", "classgestioneutente_1_1eccezioni_1_1_utente_duplicato_exception.html", null ],
+        [ "gestioneutente.eccezioni.UtenteMailException", "classgestioneutente_1_1eccezioni_1_1_utente_mail_exception.html", null ],
+        [ "gestioneutente.eccezioni.UtenteNomeCognomeException", "classgestioneutente_1_1eccezioni_1_1_utente_nome_cognome_exception.html", null ]
+      ] ],
+      [ "gestioneutente.eccezioni.UtentePrestitoAttivoException", "classgestioneutente_1_1eccezioni_1_1_utente_prestito_attivo_exception.html", null ]
+    ] ],
+    [ "Initializable", null, [
+      [ "appbibliotecauniversitaria.ControlloreHome", "classappbibliotecauniversitaria_1_1_controllore_home.html", null ],
+      [ "gestionelibro.registrazione.ControlloreRegLibro", "classgestionelibro_1_1registrazione_1_1_controllore_reg_libro.html", null ],
+      [ "gestionelibro.visualizzazione.ControlloreVisLibri", "classgestionelibro_1_1visualizzazione_1_1_controllore_vis_libri.html", null ],
+      [ "gestioneprestito.registrazione.ControlloreRegPrestito", "classgestioneprestito_1_1registrazione_1_1_controllore_reg_prestito.html", null ],
+      [ "gestioneprestito.visualizzazione.ControlloreVisPrestiti", "classgestioneprestito_1_1visualizzazione_1_1_controllore_vis_prestiti.html", null ],
+      [ "gestioneutente.registrazione.ControlloreRegUtente", "classgestioneutente_1_1registrazione_1_1_controllore_reg_utente.html", null ],
+      [ "gestioneutente.visualizzazione.ControlloreVisUtenti", "classgestioneutente_1_1visualizzazione_1_1_controllore_vis_utenti.html", null ]
+    ] ],
+    [ "Serializable", null, [
+      [ "gestionelibro.Libro", "classgestionelibro_1_1_libro.html", null ],
+      [ "gestioneprestito.Prestito", "classgestioneprestito_1_1_prestito.html", null ],
+      [ "gestioneutente.Utente", "classgestioneutente_1_1_utente.html", null ]
+    ] ],
+    [ "gestioneprestito.Stato", "enumgestioneprestito_1_1_stato.html", null ]
+];
