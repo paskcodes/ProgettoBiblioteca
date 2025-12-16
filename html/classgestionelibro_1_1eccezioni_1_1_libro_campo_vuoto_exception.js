766bd5405ddcbfb2a7c765e5a4fe436f72745fa5
Serefzz <antoniofrancogg@gmail.com>
2025-12-16 08:17:59 +0100

    Prima parte classe di test Utente

---
+++ html/classgestionelibro_1_1eccezioni_1_1_libro_campo_vuoto_exception.js
@@ -0,0 +1,5 @@
+var classgestionelibro_1_1eccezioni_1_1_libro_campo_vuoto_exception =
+[
+    [ "LibroCampoVuotoException", "classgestionelibro_1_1eccezioni_1_1_libro_campo_vuoto_exception.html#ad0e27d2e05636988f2c090f798823a09", null ],
+    [ "LibroCampoVuotoException", "classgestionelibro_1_1eccezioni_1_1_libro_campo_vuoto_exception.html#a03e4af500484bd66b83e51c225fda777", null ]
+];
---
+++ html/classgestioneutente_1_1eccezioni_1_1_utente_matricola_exception.js
@@ -0,0 +1,5 @@
+var classgestioneutente_1_1eccezioni_1_1_utente_matricola_exception =
+[
+    [ "UtenteMatricolaException", "classgestioneutente_1_1eccezioni_1_1_utente_matricola_exception.html#a401a9818dd4a689894519552af68fd3a", null ],
+    [ "UtenteMatricolaException", "classgestioneutente_1_1eccezioni_1_1_utente_matricola_exception.html#a483d293e682c0712057ee9c873139466", null ]
+];
